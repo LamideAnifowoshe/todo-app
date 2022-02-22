e06f08e9ae396240a547a7b2104cf49234062734
Olamide Anifowoshe <74327981+LamideAnifowoshe@users.noreply.github.com>
2022-02-22 19:55:08 +0100

    update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,8 +3,8 @@ import React from "react";
 const Header = () => {
   return (
     <div>
-      <h1 className="pt-5">Good morning, Jason</h1>
-      <h5>Here Are Your Tasks For Today</h5>
+      <h1>Good morning, Jason</h1>
+      <h5 className="ps-4">Here Are Your Tasks For Today</h5>
     </div>
   );
 };
