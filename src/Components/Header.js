8e9cdd8e906e72f36de9dc5ea54111a9b3211cfd
Olamide Anifowoshe <74327981+LamideAnifowoshe@users.noreply.github.com>
2022-02-22 09:33:24 +0100

    Create Header Component

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <h1 className="pt-5">Good morning, Jason</h1>
+      <h5>Here Are Your Tasks For Today</h5>
+    </div>
+  );
+};
+
+export default Header;
