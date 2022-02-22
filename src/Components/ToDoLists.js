a3cbd1f165e6e87794af17eaf1042df1417dc5bb
Olamide Anifowoshe <74327981+LamideAnifowoshe@users.noreply.github.com>
2022-02-22 10:13:40 +0100

    update ToDoLists.js

--- src/Components/ToDoLists.js
+++ src/Components/ToDoLists.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Todos= [
     {
         id: 0
-        Date: "14th November"
+        Date: "14 November"
         Activity: "Play Chess with Messi"
         Start Time: "12:51pm"
         End Time: "1:30pm"
@@ -11,30 +11,30 @@ const Todos= [
     },
 
     {
-        id: 0
-        Date: "14th November"
-        Activity: "Play Chess with Messi"
-        Start Time: "12:51pm"
-        End Time: "1:30pm"
-        Duration: "39 mins"
+        id: 1
+        Date: "02 December"
+        Activity: "Go Skydiving with Kal-el"
+        Start Time: "5:50pm"
+        End Time: "6:00pm"
+        Duration: "10 mins"
     },
 
     {
         id: 0
-        Date: "14th November"
-        Activity: "Play Chess with Messi"
-        Start Time: "12:51pm"
-        End Time: "1:30pm"
-        Duration: "39 mins"
+        Date: "08 December"
+        Activity: "Visit Mum"
+        Start Time: "08:00pm"
+        End Time: "11:00pm"
+        Duration: "03 hrs"
     },
 
     {
         id: 0
-        Date: "14th November"
-        Activity: "Play Chess with Messi"
-        Start Time: "12:51pm"
-        End Time: "1:30pm"
-        Duration: "39 mins"
+        Date: "25 December"
+        Activity: "Christmas at Sue's"
+        Start Time: "10:30am"
+        End Time: "12:00pm"
+        Duration: "1hr 30mins"
     },
 ]
 
