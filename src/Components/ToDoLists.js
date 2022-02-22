5c1dcc26062298035965ce7724ac92db1898917a
Olamide Anifowoshe <74327981+LamideAnifowoshe@users.noreply.github.com>
2022-02-22 09:34:05 +0100

    Create ToDoLists Components

---
+++ src/Components/ToDoLists.js
@@ -0,0 +1,47 @@
+import React from 'react'
+
+const Todos= [
+    {
+        id: 0
+        Date: "14th November"
+        Activity: "Play Chess with Messi"
+        Start Time: "12:51pm"
+        End Time: "1:30pm"
+        Duration: "39 mins"
+    },
+
+    {
+        id: 0
+        Date: "14th November"
+        Activity: "Play Chess with Messi"
+        Start Time: "12:51pm"
+        End Time: "1:30pm"
+        Duration: "39 mins"
+    },
+
+    {
+        id: 0
+        Date: "14th November"
+        Activity: "Play Chess with Messi"
+        Start Time: "12:51pm"
+        End Time: "1:30pm"
+        Duration: "39 mins"
+    },
+
+    {
+        id: 0
+        Date: "14th November"
+        Activity: "Play Chess with Messi"
+        Start Time: "12:51pm"
+        End Time: "1:30pm"
+        Duration: "39 mins"
+    },
+]
+
+const ToDoLists = () => {
+  return (
+    <div>Table</div>
+  )
+}
+
+export default ToDoLists
