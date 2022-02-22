46e04a70b7268e76bf6188faa4a6368c2dfe8706
Olamide Anifowoshe <74327981+LamideAnifowoshe@users.noreply.github.com>
2022-02-22 09:32:20 +0100

    create NavBar Component

---
+++ src/Components/NavBar.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Ellipse from "./Images/Ellipse.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBell } from "@fortawesome/free-solid-svg-icons";
+
+const NavBar = () => {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-sm bg-light navbar-light">
+        <div className="container-fluid">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link" href="/">
+                <FontAwesomeIcon icon={faBell} />
+              </a>
+            </li>
+            <li className="nav-item">
+              <h4>Jason Blage</h4>
+            </li>
+          </ul>
+          <img src={Ellipse} className="navbar-brand" />
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default NavBar;
