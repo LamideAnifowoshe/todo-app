e4095ab2db5c83672792e913ace41e11f4f39cd4
Olamide Anifowoshe <74327981+LamideAnifowoshe@users.noreply.github.com>
2022-02-23 19:25:27 +0100

    Remove Modal from NewToDo.js to a new file

--- src/Components/NewToDo.js
+++ src/Components/NewToDo.js
@@ -1,14 +1,9 @@
 import React from "react";
 import { useState } from "react";
 import Button from "react-bootstrap/Button";
-import ModalBody from "react-bootstrap/ModalBody";
-import ModalTitle from "react-bootstrap/ModalTitle";
-import ModalHeader from "react-bootstrap/ModalHeader";
-import ModalDialog from "react-bootstrap/ModalDialog";
-import Modal from "react-bootstrap/Modal";
-import Form from "react-bootstrap/Form";
+import AddToDo from "./AddToDo";
 
-const NewToDo = () => {
+const NewToDo = ({ onAdd }) => {
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
@@ -30,60 +25,12 @@ const NewToDo = () => {
         }}
       />
 
-      <Modal
+      <AddToDo
+        onClose={handleClose}
+        onShow={handleShow}
         show={show}
-        onHide={handleClose}
-        backdrop="static"
-        keyboard={false}
-        size="lg"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
-      >
-        <Modal.Header closeButton>
-          <Modal.Title
-            style={{
-              backgroundColor: "#053858",
-              color: "white",
-              height: "49px",
-              width: "100%",
-              position: "absolute",
-              left: "0px",
-            }}
-          >
-            Create New Task
-          </Modal.Title>
-        </Modal.Header>
-
-        <Modal.Body>
-          <Form>
-            <Form.Group className="mb-3" controlId="formBasicText">
-              <Form.Label>Task Title</Form.Label>
-              <Form.Control type="text" placeholder="Play Chess with Messi" />
-            </Form.Group>
-
-            <Form.Group className="mb-3" controlId="formBasicText">
-              <Form.Label>Task Description</Form.Label>
-              <Form.Control
-                type="message"
-                placeholder="Show the football GOAT whuz da boss"
-              />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicCheckbox">
-              <Form.Check type="checkbox" label="Check me out" />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-              Submit
-            </Button>
-          </Form>
-        </Modal.Body>
-
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary">Understood</Button>
-        </Modal.Footer>
-      </Modal>
+        onAdd={onAdd}
+      />
     </div>
   );
 };
