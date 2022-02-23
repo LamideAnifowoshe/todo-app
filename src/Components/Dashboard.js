a83b144119606033b2e0d04cbc0a63903c6602e8
Olamide Anifowoshe <74327981+LamideAnifowoshe@users.noreply.github.com>
2022-02-23 07:54:06 +0100

    import NewToDo.js

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -4,8 +4,9 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import ToDo from "./ToDo";
 import SideNav from "./SideNav";
+import NewToDo from "./NewToDo";
 
-const Dashboard = ({ todos }) => {
+const Dashboard = ({ todos, onAdd }) => {
   return (
     <div>
       <Container fluid={true}>
@@ -17,6 +18,7 @@ const Dashboard = ({ todos }) => {
             <h5 className="ps-4">Here Are Your Tasks For Today</h5>
             <ToDo todos={todos} />
           </Col>
+          <NewToDo onAdd={onAdd} />
         </Row>
       </Container>
     </div>
