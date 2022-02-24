f6a03e55248d25b2c18d02d2df7b2946e18313ff
Olamide Anifowoshe <74327981+LamideAnifowoshe@users.noreply.github.com>
2022-02-24 03:33:36 +0100

    Update Dashboard.js

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -9,18 +9,8 @@ import NewToDo from "./NewToDo";
 const Dashboard = ({ todos, onAdd }) => {
   return (
     <div>
-      <Container fluid={true}>
-        <Row>
-          <Col sm={2}>
-            <SideNav />
-          </Col>
-          <Col sm={10}>
-            <h5 className="ps-4">Here Are Your Tasks For Today</h5>
-            <ToDo todos={todos} />
-          </Col>
-          <NewToDo onAdd={onAdd} />
-        </Row>
-      </Container>
+      <h5 className="ps-4">Here Are Your Tasks For Today</h5>
+      <ToDo todos={todos} />
     </div>
   );
 };
