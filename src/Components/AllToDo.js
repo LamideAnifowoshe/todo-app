43967847d46394e35b2bbba130a0dcd48a772d62
Olamide Anifowoshe <74327981+LamideAnifowoshe@users.noreply.github.com>
2022-02-24 08:11:13 +0100

    Update AllToDo.js , import react bootstrap tab

--- src/Components/AllToDo.js
+++ src/Components/AllToDo.js
@@ -1,7 +1,42 @@
 import React from "react";
+import ToDo from "./ToDo";
+import Tab from "react-bootstrap/Tab";
+import Nav from "react-bootstrap/Nav";
+import PastTasks from "./PastTasks";
 
-const AllToDo = () => {
-  return <div>Hello Big Head Favor!</div>;
+const AllToDo = ({ todos }) => {
+  return (
+    <div>
+      <Tab.Container defaultActiveKey="first">
+        <Nav
+          variant="pills"
+          className="flex-column"
+          n
+          style={{
+            marginTop: "263px",
+            width: "20%",
+            backgroundColor: "#053858 !important",
+          }}
+        >
+          <Nav.Item>
+            <Nav.Link eventKey="first">Upcoming Tasks</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="second">PastTasks</Nav.Link>
+          </Nav.Item>
+        </Nav>
+
+        <Tab.Content>
+          <Tab.Pane eventKey="first">
+            <ToDo todos={todos} />
+          </Tab.Pane>
+          <Tab.Pane eventKey="second">
+            <PastTasks />
+          </Tab.Pane>
+        </Tab.Content>
+      </Tab.Container>
+    </div>
+  );
 };
 
 export default AllToDo;
