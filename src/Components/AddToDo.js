84026e2587b0ab2ad1d6cb01c13ef840f78f913d
Olamide Anifowoshe <74327981+LamideAnifowoshe@users.noreply.github.com>
2022-02-24 18:04:05 +0100

    Update AddToDo.js

--- src/Components/AddToDo.js
+++ src/Components/AddToDo.js
@@ -1,6 +1,9 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
 import { useState } from "react";
 
@@ -46,68 +49,107 @@ const AddToDo = ({ onClose, onShow, show, onAdd }) => {
         </Modal.Header>
 
         <Modal.Body>
-          <Form onSubmit={onSubmit}>
-            <Form.Group className="mb-3" controlId="formBasicText">
-              <Form.Label>Date</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Play Chess with Messi"
-                value={date}
-                onChange={(e) => setDate(e.target.value)}
-              />
-              <Form.Label>Start Time</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="12:05pm"
-                value={start}
-                onChange={(e) => setStart(e.target.value)}
-              />
-              <Form.Label>End Time</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="1:30pm"
-                value={end}
-                onChange={(e) => setEnd(e.target.value)}
-              />
-            </Form.Group>
+          <Container>
+            <Form onSubmit={onSubmit}>
+              <Row>
+                <Col sm={8}>
+                  <Form.Group className="mb-3" controlId="formBasicText">
+                    <Form.Label>Date</Form.Label>
+                    <Form.Control
+                      type="text"
+                      placeholder="Play Chess with Messi"
+                      value={date}
+                      onChange={(e) => setDate(e.target.value)}
+                    />
+                  </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicText">
-              <Form.Label>Task Description</Form.Label>
-              <Form.Control
-                type="textarea"
-                placeholder="Show the football GOAT whuz da boss"
-                value={activity}
-                onChange={(e) => setActivity(e.target.value)}
-                style={{ height: "100px" }}
-              />
-            </Form.Group>
+                  <Form.Group className="mb-3" controlId="formBasicText">
+                    <Form.Label>Task Description</Form.Label>
+                    <Form.Control
+                      type="textarea"
+                      placeholder="Show the football GOAT whuz da boss"
+                      value={activity}
+                      onChange={(e) => setActivity(e.target.value)}
+                      style={{ height: "100px" }}
+                    />
+                  </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicText">
-              <Form.Label>Sub-Task</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Play 1st round Chess with Messi"
-                value={subactivity}
-                onChange={(e) => setSubActivity(e.target.value)}
-              />
-            </Form.Group>
+                  <Form.Group className="mb-3" controlId="formBasicText">
+                    <Form.Label>Sub-Task</Form.Label>
+                    <Form.Control
+                      type="text"
+                      placeholder="Play 1st round Chess with Messi"
+                      value={subactivity}
+                      onChange={(e) => setSubActivity(e.target.value)}
+                    />
+                  </Form.Group>
+                  <Button
+                    variant="primary"
+                    style={{
+                      backgroundColor: "#FFFFFF",
+                      borderWidth: "2px",
+                      borderStyle: "solid",
+                      borderColor: "#053858",
+                      color: "#053858",
+                      boxSizing: "border-box",
+                      borderRadius: "30px",
+                      width: "204px",
+                    }}
+                  >
+                    Add Sub-task
+                  </Button>
+                </Col>
 
-            <Form.Group className="mb-3" controlId="formBasicText">
-              <Form.Label>Sub-Task Duration</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="None"
-                value={duration}
-                onChange={(e) => setDuration(e.target.value)}
-              />
-            </Form.Group>
-            <Button variant="primary">Add Sub-task</Button>
-            <Modal.Footer>
-              <Button variant="secondary" type="submit" onClick={onSubmit}>
-                Save
-              </Button>
-            </Modal.Footer>
-          </Form>
+                <Col sm={2}>
+                  <Form.Group className="mb-3" controlId="formBasicText">
+                    <Form.Label>Start Time</Form.Label>
+                    <Form.Control
+                      type="text"
+                      placeholder="12:05pm"
+                      value={start}
+                      onChange={(e) => setStart(e.target.value)}
+                    />
+                  </Form.Group>
+                </Col>
+
+                <Col sm={2}>
+                  <Form.Group className="mb-3" controlId="formBasicText">
+                    <Form.Label>End Time</Form.Label>
+                    <Form.Control
+                      type="text"
+                      placeholder="1:30pm"
+                      value={end}
+                      onChange={(e) => setEnd(e.target.value)}
+                    />
+                  </Form.Group>
+
+                  <Form.Group className="mb-3" controlId="formBasicText">
+                    <Form.Label>Duration</Form.Label>
+                    <Form.Control
+                      type="text"
+                      placeholder="None"
+                      value={duration}
+                      onChange={(e) => setDuration(e.target.value)}
+                    />
+                  </Form.Group>
+                </Col>
+                <Modal.Footer>
+                  <Button
+                    variant="secondary"
+                    type="submit"
+                    onClick={onSubmit}
+                    style={{
+                      backgroundColor: "#053858",
+                      borderRadius: "30px",
+                      width: "144px",
+                    }}
+                  >
+                    Save
+                  </Button>
+                </Modal.Footer>
+              </Row>
+            </Form>
+          </Container>
         </Modal.Body>
       </Modal>
     </div>
