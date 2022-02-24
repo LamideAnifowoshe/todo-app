543a26d32313961f2aa8891999def0240d7dd8f1
Olamide Anifowoshe <74327981+LamideAnifowoshe@users.noreply.github.com>
2022-02-24 08:10:15 +0100

    Update Dashboard.js

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,7 +1,4 @@
 import React from "react";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 import ToDo from "./ToDo";
 import SideNav from "./SideNav";
 import NewToDo from "./NewToDo";
