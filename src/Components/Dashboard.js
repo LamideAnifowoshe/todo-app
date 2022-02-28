993e75ccf5882572b3f510e4ccf2b54d2cfe436b
Olamide Anifowoshe <74327981+LamideAnifowoshe@users.noreply.github.com>
2022-02-28 23:16:24 +0100

    Update Dashboard.js

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -6,7 +6,12 @@ import NewToDo from "./NewToDo";
 const Dashboard = ({ todos, onAdd, onToggle, todo, onDelete }) => {
   return (
     <div>
-      <h5 className="ps-4">Here Are Your Tasks For Today</h5>
+      {todos.length === 0 ? (
+        <h5 className="ps-4">No Task Available</h5>
+      ) : (
+        <h5 className="ps-4">Here Are Your Tasks For Today</h5>
+      )}
+
       <ToDo todos={todos} onToggle={onToggle} todo={todo} onDelete={onDelete} />
     </div>
   );
